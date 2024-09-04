ea9af6edada5144c75eed9a7e94e1ae7ad29a47c
levanlevi <l.khunjgurua@gmail.com>
2024-09-04 13:24:11 +0400

    first

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const Footer = memo(() => {
+    return (
+        <footer>
+            footer
+        </footer>
+    );
+});
+
+export default Footer;
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,7 @@
+import React, { memo } from 'react';
+
+const Header = memo(() => {
+    return <header>header</header>;
+});
+
+export default Header;
