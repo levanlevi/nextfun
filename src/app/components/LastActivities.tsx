fae1bd64c5539b07f569a2c8263d17adc2042869
levanlevi <l.khunjgurua@gmail.com>
2024-09-05 21:00:33 +0400

    subscription with cursor

--- src/app/components/LastActivities.tsx
+++ src/app/components/LastActivities.tsx
@@ -1,6 +1,6 @@
 import { Badge, Table, Text } from "@radix-ui/themes";
 import { ExternalLinkIcon } from '@radix-ui/react-icons'
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { gql, useSubscription } from "@apollo/client";
 import { Activity } from "@/types/Activity";
@@ -9,35 +9,64 @@ import { formatDate, formatTXID } from "@/utils/formatting";
 import CopyButton from "./common/CopyButton";
 
 const GET_LOGS = gql`
-    subscription ActivityLogsSubscription {
-        logs_stream(batch_size: 10, cursor: {initial_value: {block_number: "0"}}) {
+    subscription ActivityLogsSubscription(
+            $batch_size: Int! = 5, 
+            $cursor: [logs_stream_cursor_input]!) {
+        logs_stream(
+            batch_size: $batch_size,
+            cursor: $cursor
+        ) {
             transaction_hash
             block_timestamp
             address
             decoded(path: "amount0In")
-            block_number
         }
     }
 `;
 
 const LastActivities = () => {
+    // This is a hack to get the last 50 seconds of logs for smooth demo app
+    const nowMinus50Secs = new Date(new Date().getTime() - 45 * 1000);
+    const [lastBlockTimeStamp, setLastBlockTimeStamp] = useState(nowMinus50Secs.toISOString());
+    const [activities, setActivities] = useState<Activity[]>([]);
     const { data, loading, error } =
-        useSubscription<LogsStreamSubscriptionResponse>(GET_LOGS);
+        useSubscription<LogsStreamSubscriptionResponse>(GET_LOGS, {
+            variables: {
+                cursor: {
+                    initial_value: {
+                        block_timestamp: lastBlockTimeStamp
+                    },
+                    ordering: 'ASC'
+                },
+                batch_size: 2
+            },
+            onSubscriptionData: ({ subscriptionData }) => {
+                if (!subscriptionData.data) {
+                    return;
+                }
+                console.log(subscriptionData.data);
+                const logs = subscriptionData.data.logs_stream;
+
+                if (logs.length > 0) {
+                    setLastBlockTimeStamp(logs[logs.length - 1].block_timestamp);
+                }
+                const newActivities = logs.map((log: LogsStream) => {
+                    return {
+                        activity: 'Swap',
+                        points: log.decoded,
+                        date: log.block_timestamp,
+                        txId: log.transaction_hash,
+                        link: '#'
+                    }
+                }) ?? [];
+                setActivities(newActivities);
+            }
+        });
 
     // todo fix review
-    if (loading) return <p>Loading...</p>;
-    // todo fix review
-    if (error) return <p>Error: {error.message}</p>;
-
-    const activities: Activity[] = data?.logs_stream.map((log: LogsStream) => {
-        return {
-            activity: 'Swap',
-            points: log.decoded,
-            date: log.block_timestamp,
-            txId: log.transaction_hash,
-            link: '#'
-        }
-    }) ?? [];
+    if (error) {
+        return <p>Error: {error.message}</p>;
+    }
 
     return (
         <Table.Root
@@ -54,7 +83,7 @@ const LastActivities = () => {
             </Table.Header>
 
             <Table.Body>
-                {activities.map((activity) => (
+                {activities && activities.length > 0 && activities.map((activity) => (
                     <Table.Row key={activity.txId}>
                         <Table.Cell>
                             {activity.activity}
@@ -74,7 +103,7 @@ const LastActivities = () => {
                         </Table.Cell>
                         <Table.Cell>
                             <div className="flex items-center space-x-2">
-                                <Text className="md:w-[calc(12ch+1.5rem)] truncate">
+                                <Text className="md:w-[calc(11ch+0.5rem)] truncate">
                                     {formatTXID(activity.txId)}
                                 </Text>
                                 <CopyButton textToCopy={activity.txId} />
