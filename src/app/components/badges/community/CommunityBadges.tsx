35d9999a843f9e5f89fbf6eacf1bfb9a958f1f29
levanlevi <l.khunjgurua@gmail.com>
2024-09-07 16:22:54 +0400

    community badges respons

--- src/app/components/badges/community/CommunityBadges.tsx
+++ src/app/components/badges/community/CommunityBadges.tsx
@@ -13,7 +13,8 @@ const BadgeCards = (props: PropType) => {
             <Box className="px-5 py-3 bg-background-elevation-3 rounded-t-lg">
                 <h2 className="font-medium">Community Badges</h2>
             </Box>
-            <Box className="flex overflow-x-auto space-x-4 p-5 bg-background-elevation-1 rounded-b-lg scrollbar-thin scrollbar-thumb-primary-hover">
+            <Box className="flex w-full flex-wrap overflow-x-auto space-x-4 p-5 bg-background-elevation-1 rounded-b-lg scrollbar-thin scrollbar-thumb-primary-hover
+            sm:flex-nowrap">
                 {props.badgeList.map((badge, index) => (
                     <Box key={index} className="flex-shrink-0">
                         <BadgeCard badge={badge} onClick={props.onBadgeSelect} />
