a4a91a42eb6dc305f499d6384fbacc6dc482e376
levanlevi <l.khunjgurua@gmail.com>
2024-09-07 11:38:04 +0400

    carousel work

--- src/app/components/badges/carousel/Carousel.tsx
+++ src/app/components/badges/carousel/Carousel.tsx
@@ -11,7 +11,7 @@ import { useState } from "react";
 
 const Carousel = () => {
     const badgeList: BadgesData = badgesJson as BadgesData;
-    // const activeIndex = Math.floor(badgeList.length / 2);
+    const [activeIndex, setActiveIndex] = useState(Math.floor(badgeList.length / 2));
     const OPTIONS: EmblaOptionsType = { loop: false };
     const [goPrev, setGoPrev] = useState(false);
     const [goNext, setGoNext] = useState(false);
@@ -19,56 +19,61 @@ const Carousel = () => {
     const handlePrev = () => {
         setGoPrev(!goPrev);
     }
+
     const handleNext = () => {
         setGoNext(!goNext);
+    }
+
+    const getSlideCards = () => {
+        return badgeList.map((item, index) => (
+            <Box className="embla__slide__badgecard" key={index}>
+                {
+                    (index === activeIndex) ? (
+                        <>
+                            <BadgeCard
+                                id="pudgyOg"
+                                title={item.title}
+                                multiplier={item.multiplier}
+                                description={item.description}
+                                selected={true}>
+                            </BadgeCard>
+                            <Box className="bg-background-elevation-2 rounded-lg w-[176px] py-1 px-4 mt-2 flex flex-col">
+                                <Text className="text-text-primary text-xs font-medium text-center">
+                                    Reward Details
+                                </Text>
+                                <Text className="text-text-secondary text-xs font-medium text-center">
+                                    Liquidity Provision to ETH/USDC
+                                </Text>
+                            </Box>
+                        </>
+                    ) : (
+                        <BadgeCardSkeleton>
+                        </BadgeCardSkeleton>
+                    )
+                }
+            </Box>
+        ))
     }
 
     return (
         <>
             <Box className="flex flex-row justify-between">
-                <Box className="bg-background-elevation-3 hover:bg-background-elevation-2 cursor-pointer flex items-center justify-center rounded-xl px-3" onClick={handlePrev}>
+                <Box className="bg-background-elevation-3 hover:bg-background-elevation-2 cursor-pointer flex items-center justify-center rounded-xl px-3"
+                    onClick={handlePrev}>
                     <Image src={arrowPrev} width={8} height={8} alt="Prev" />
                 </Box>
 
                 <Box className="flex flex-row space-x-2 overflow-hidden">
-                    {
-                        <EmblaCarousel
-                            goNext={goNext}
-                            goPrev={goPrev}
-                            options={OPTIONS}
-                            slides={
-                                badgeList.map((item, index) => (
-                                    <Box className="bzz" key={index}>
-                                        {
-                                            (index === -12) ? (
-                                                <>
-                                                    <BadgeCard
-                                                        id="pudgyOg"
-                                                        title="some hey"
-                                                        multiplier={2.1}
-                                                        description="some description"
-                                                        selected={false}>
-                                                    </BadgeCard>
-                                                    <Box className="bg-background-elevation-2 rounded-lg w-[176px] py-1 px-4 mt-2 flex flex-col">
-                                                        <Text className="text-text-primary text-xs font-medium text-center">
-                                                            Reward Details
-                                                        </Text>
-                                                        <Text className="text-text-secondary text-xs font-medium text-center">
-                                                            Liquidity Provision to ETH/USDC
-                                                        </Text>
-                                                    </Box>
-                                                </>
-                                            ) : (
-                                                <BadgeCardSkeleton>
-                                                </BadgeCardSkeleton>
-                                            )
-                                        }
-                                    </Box>
-                                ))} />
-                    }
+                    {<EmblaCarousel
+                        onActiveIndexChange={(index) => setActiveIndex(index)}
+                        goNext={goNext}
+                        goPrev={goPrev}
+                        options={OPTIONS}
+                        slides={getSlideCards()} />}
                 </Box>
 
-                <Box className="bg-background-elevation-3 hover:bg-background-elevation-2 cursor-pointer flex items-center justify-center rounded-xl px-3" onClick={handleNext}>
+                <Box className="bg-background-elevation-3 hover:bg-background-elevation-2 cursor-pointer flex items-center justify-center rounded-xl px-3"
+                    onClick={handleNext}>
                     <Image src={arrowNext} width={8} height={8} alt="Next" />
                 </Box>
             </Box>
--- src/app/components/badges/carousel/CustomCarousel.tsx
+++ src/app/components/badges/carousel/CustomCarousel.tsx
@@ -1,55 +1,52 @@
-import React, { useCallback, useEffect, useRef } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import {
   EmblaCarouselType,
   EmblaEventType,
   EmblaOptionsType
 } from 'embla-carousel'
 import useEmblaCarousel from 'embla-carousel-react'
-import {
-  NextButton,
-  PrevButton,
-  usePrevNextButtons
-} from './EmblaCarouselArrowButtons'
-import { DotButton, useDotButton } from './EmplaCarouselDotButton'
-
-const TWEEN_FACTOR_BASE = 0.52
-
-const numberWithinRange = (number: number, min: number, max: number): number =>
-  Math.min(Math.max(number, min), max)
+import { useDotButton } from './EmplaCarouselDotButton'
+import { usePrevNextButtons } from './EmblaCarouselArrowButtons'
 
 type PropType = {
   slides: any[]
   options?: EmblaOptionsType,
   goPrev?: boolean,
   goNext?: boolean,
+  onActiveIndexChange?: (index: number) => void
 }
 
-const EmblaCarousel: React.FC<PropType> = (props) => {
-  const { slides, options } = props
-  const [emblaRef, emblaApi] = useEmblaCarousel(options)
-  const tweenFactor = useRef(0)
-  const tweenNodes = useRef<HTMLElement[]>([])
+const TWEEN_FACTOR_BASE = 0.84
 
-  const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi)
+const numberWithinRange = (number: number, min: number, max: number): number =>
+  Math.min(Math.max(number, min), max)
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const tweenFactor = useRef(0);
+  const tweenNodes = useRef<HTMLElement[]>([]);
+
+  const { selectedIndex } =
+    useDotButton(emblaApi);
+
+  useEffect(() => {
+    console.log('selectedIndex Changed:', selectedIndex);
+    if (props.onActiveIndexChange) {
+      props.onActiveIndexChange(selectedIndex);
+    }
+  }, [selectedIndex]);
 
   const {
     prevBtnDisabled,
     nextBtnDisabled,
     onPrevButtonClick,
     onNextButtonClick
-  } = usePrevNextButtons(emblaApi)
-
-  const setTweenNodes = useCallback((emblaApi: EmblaCarouselType): void => {
-    tweenNodes.current = emblaApi.slideNodes()
-      .map((slideNode) => {
-        return slideNode as HTMLElement
-      })
-  }, [])
+  } = usePrevNextButtons(emblaApi);
 
   const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
     tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length
-  }, [])
+  }, []);
 
   const tweenScale = useCallback(
     (emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
@@ -57,106 +54,65 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
       const scrollProgress = emblaApi.scrollProgress()
       const slidesInView = emblaApi.slidesInView()
       const isScrollEvent = eventName === 'scroll'
-      const selectedIndex = emblaApi.selectedScrollSnap();
 
-      emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
-        let diffToTarget = scrollSnap - scrollProgress
+      emblaApi
+        .scrollSnapList()
+        .forEach(
+          (scrollSnap, snapIndex) => {
+            let diffToTarget = scrollSnap - scrollProgress
+            const slidesInSnap = engine.slideRegistry[snapIndex]
 
-        const slidesInSnap = engine.slideRegistry[snapIndex]
+            slidesInSnap?.forEach(slideIndex => {
+              if (isScrollEvent && !slidesInView.includes(slideIndex)) {
+                return;
+              }
 
-        slidesInSnap.forEach((slideIndex) => {
-          if (isScrollEvent && !slidesInView.includes(slideIndex)) {
-            return;
-          }
-          const tweenNode = tweenNodes.current[slideIndex]
-          if (!tweenNode) {
-            return;
-          }
-
-          if (selectedIndex === snapIndex) {
-            tweenNode.style.transform = `scale(1)`;
-            return;
-          }
-
-          const coefficient = selectedIndex === 0 ? 0.2 : selectedIndex;
-          const scale = Math.pow(0.837, Math.abs(coefficient - snapIndex));
-          console.log('selectedIndex', selectedIndex,'coefficient',coefficient, 'snapIndex', snapIndex, scale);
-          tweenNode.style.transform = `scale(${scale})`;
-
-          // const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current)
-          // const scale = numberWithinRange(tweenValue, 0, 1).toString()
-          // const tweenNode = tweenNodes.current[slideIndex]
-          // if (!tweenNode) {
-          //   console.log('-----------------------END---------------------------');
-          //   return;
-          // }
-          // console.log('tweenValue', tweenValue);
-          // console.log('tweenNode', tweenNode);
-          // console.log('scale', scale);
-
-          // tweenNode.style.transform = `scale(${scale})`
-          // console.log('-----------------------END---------------------------');
-        });
-
-      })
+              const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current)
+              const scale = numberWithinRange(tweenValue, 0, 1).toString()
+              const tweenNode = tweenNodes.current[slideIndex]
+              if (tweenNode) {
+                tweenNode.style.transform = `scale(${scale})`
+              }
+            });
+          });
     },
-    []
+    [],
   )
-
-  useEffect(() => {
-    if (emblaApi) {
-      emblaApi.scrollPrev();
-    }
-  }, [props.goPrev]);
-
-  useEffect(() => {
-    if (emblaApi) {
-      emblaApi.scrollNext();
-    }
-  }, [props.goNext]);
 
   useEffect(() => {
     if (!emblaApi) return
 
-    setTweenNodes(emblaApi)
     setTweenFactor(emblaApi)
     tweenScale(emblaApi)
-
     emblaApi
-      .on('reInit', setTweenNodes)
       .on('reInit', setTweenFactor)
       .on('reInit', tweenScale)
       .on('scroll', tweenScale)
       .on('slideFocus', tweenScale)
   }, [emblaApi, tweenScale])
 
+  useEffect(() => {
+    if (!emblaApi) {
+      return;
+    }
+    emblaApi.scrollPrev();
+  }, [props.goPrev]);
+
+  useEffect(() => {
+    if (!emblaApi) {
+      return;
+    }
+    emblaApi.scrollNext();
+  }, [props.goNext]);
+
   return (
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {slides.map((slide, index) => (
-            <div className={`embla__slide ${index === selectedIndex ? 'selected' : ''}`} key={index}>
+            <div className="embla__slide" key={index}>
               {slide}
             </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="embla__controls">
-        <div className="embla__buttons">
-          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-        </div>
-
-        <div className="embla__dots">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={'embla__dot'.concat(
-                index === selectedIndex ? ' embla__dot--selected' : ''
-              )}
-            />
           ))}
         </div>
       </div>
@@ -164,4 +120,4 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
   )
 }
 
-export default EmblaCarousel;
+export default EmblaCarousel
