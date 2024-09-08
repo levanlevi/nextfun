b5d22c3f4ed6c6c6b8ea4011b2bea0b76dfbbff1
levanlevi <l.khunjgurua@gmail.com>
2024-09-08 17:07:37 +0400

    header sm

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,74 +1,97 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { logo, profileIcon } from './../../../public/';
 import { Box } from '@radix-ui/themes';
 
 const Header = memo(() => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
     return (
-        <header className="flex flex-col sm:flex-row items-center justify-between p-4 px-16 sm:px-18 md:px-20 lg:px-24 xl:px-28
-         border-b border-background-elevation-2 mb-10 space-x-1">
-            <div className="flex items-center justify-between w-full sm:w-auto mb-4 sm:mb-0">
-                <Link href="/">
+        <header className="flex flex-col sm:flex-row items-center p-4 px-16 sm:px-18 md:px-20 lg:px-24 xl:px-28
+         border-b border-background-elevation-2 mb-10">
+            <Box>
+                <Link className='' href="/">
                     <Image
-                        className="mr-10 sm:mr-6"
+                        className=""
                         src={logo}
                         alt="Logo"
                         height={32}
                         width={32}
                     />
                 </Link>
+            </Box>
 
-                <nav className="w-full sm:w-auto">
-                    <ul className="flex flex-wrap sm:flex-nowrap justify-between sm:justify-start space-y-2 sm:space-y-0 sm:space-x-6">
-                        <li>
-                            <Link href="/dashboard" className="font-semibold text-sm hover:text-primary-hover">
-                                Dashboard
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/tasks" className="font-semibold text-sm hover:text-primary-hover">
-                                Tasks
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/badges" className="font-semibold text-sm hover:text-primary-hover">
-                                Badges
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/leaderboard" className="font-semibold text-sm hover:text-primary-hover">
-                                Leaderboard
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/connections" className="font-semibold text-sm hover:text-primary-hover">
-                                Connections
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            <Box id="navbar-header" className='flex flex-row justify-between items-center w-full ml-4'>
+                <div className="flex items-center w-full sm:w-auto mb-4 sm:mb-0">
+                    <nav className={`w-full transition-transform duration-300 md:static md:flex md:w-auto md:space-x-6 ${isMenuOpen ? 'translate-x-0' : 'translate-x-150'} md:translate-x-0`}>
+                        <ul className='flex flex-col sm:flex-row sm:my-2 sm:space-x-0'>
+                            <li className='text-center'>
+                                <Link href="/dashboard pl-4" className="font-semibold text-sm hover:text-primary-hover">
+                                    Dashboard
+                                </Link>
+                            </li>
+                            <li className="text-center pl-4">
+                                <Link href="/tasks" className="font-semibold text-sm hover:text-primary-hover">
+                                    Tasks
+                                </Link>
+                            </li>
+                            <li className="text-center pl-4">
+                                <Link href="/badges" className="font-semibold text-sm hover:text-primary-hover">
+                                    Badges
+                                </Link>
+                            </li>
+                            <li className="text-center pl-4">
+                                <Link href="/leaderboard" className="font-semibold text-sm hover:text-primary-hover">
+                                    Leaderboard
+                                </Link>
+                            </li>
+                            <li className="text-center pl-4">
+                                <Link href="/connections" className="font-semibold text-sm hover:text-primary-hover">
+                                    Connections
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
 
-            <div className='flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2'>
-                <Box className="bg-background-elevation-2 text-text-secondary cursor-pointer hover:bg-background-elevation-3 text-center p-2 border-2 border-primary-hover rounded-3xl font-semibold">
-                    How It Works
-                </Box>
+                {!isMenuOpen && (<button
+                    onClick={toggleMenu}
+                    aria-expanded={isMenuOpen}
+                    data-collapse-toggle="navbar-header"
+                    type="button"
+                    aria-controls="navbar-header"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>)}
 
-                <Box className="bg-background-elevation-3 cursor-pointer hover:bg-background-elevation-2 rounded-3xl text-center p-2">
-                    <Box className="flex items-center space-x-1">
-                        <Image
-                            src={profileIcon}
-                            alt="Profile Picture"
-                            width={18}
-                            height={18}
-                            className="rounded-full" />
-                        <span className="font-semibold text-text-primary">
-                            user.name
-                        </span>
+                <div className='flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2 hidden md:flex'>
+                    <Box className="bg-background-elevation-2 text-text-secondary cursor-pointer hover:bg-background-elevation-3 text-center p-2 border-2 border-primary-hover rounded-3xl font-semibold">
+                        How It Works
                     </Box>
-                </Box>
-            </div>
+
+                    <Box className="bg-background-elevation-3 cursor-pointer hover:bg-background-elevation-2 rounded-3xl text-center p-2">
+                        <Box className="flex items-center space-x-1">
+                            <Image
+                                src={profileIcon}
+                                alt="Profile Picture"
+                                width={18}
+                                height={18}
+                                className="rounded-full" />
+                            <span className="font-semibold text-text-primary">
+                                user.name
+                            </span>
+                        </Box>
+                    </Box>
+                </div>
+            </Box>
         </header>
     )
 });
